d864bbb2632768333d4d0c19d0b978c586744b85
JCompile <Savenko126t@yandex.ru>
2018-10-22 21:59:08 +0300

    de

--- v2.js
+++ v2.js
@@ -18,8 +18,8 @@ class OtherwiseController extends TelegramBaseController {
             )
         }
         if ($._message._photo) {//<-----------
-            console.log($)
-            tg.api.sendPhoto($.user_id, 'AgADAgADhakxGwM5cEp_2ucxVK2aK2bMtw4ABAenTc5CODPNIYEFAAEC')
+            console.log($._userId)
+            tg.api.sendPhoto($._userId, 'AgADAgADhakxGwM5cEp_2ucxVK2aK2bMtw4ABAenTc5CODPNIYEFAAEC')
         }
 
     }

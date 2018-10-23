4fcd8762bf7814326b489a734b1a08c4528d8e01
JCompile <Savenko126t@yandex.ru>
2018-10-23 21:41:09 +0300

    цв

--- data.module.js
+++ data.module.js
@@ -89,3 +89,10 @@ module.exports.post_free_admin = {
   }),
   parse_mode: "HTML"
 };
+module.exports.company = 'Наша компания занимается тонированием и бронирование машин'
+module.exports.copmOpt= {
+  reply_markup: JSON.stringify({
+    "inline_keyboard": [[{ "text": "Узнать подробнее","url":'https://medium.com/@savenko136t/%D0%B4%D0%BB%D1%8F-%D1%86%D0%B5%D0%BD%D0%B8%D1%82%D0%B5%D0%BB%D0%B5%D0%B9-%D0%BE%D1%80%D0%B8%D0%B3%D0%B8%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8-24dd3eab2b65' }],
+ })
+};
+
--- v2.js
+++ v2.js
@@ -85,7 +85,7 @@ class Qwest extends TelegramBaseController {
 class Company extends TelegramBaseController {
     qCompany($) { 
         //analytic.add('О компании', $); 
-        $.sendMessage('https://medium.com/@savenko136t/%D0%B4%D0%BB%D1%8F-%D1%86%D0%B5%D0%BD%D0%B8%D1%82%D0%B5%D0%BB%D0%B5%D0%B9-%D0%BE%D1%80%D0%B8%D0%B3%D0%B8%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8-24dd3eab2b65')
+        $.sendMessage(data.company,data.copmOpt)
     }
     get routes() { return { 'company': 'qCompany' } }
 }

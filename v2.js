c5e767bef7bd112b251aea216035a8e63ec4de86
JCompile <Savenko126t@yandex.ru>
2018-10-23 06:29:18 +0300

    d

--- v2.js
+++ v2.js
@@ -121,7 +121,7 @@ class SaleController extends TelegramBaseController {
 class Company extends TelegramBaseController {
     qCompany($) { 
         //analytic.add('О компании', $); 
-        $.sendMessage(data.company,{url:'https://medium.com/@savenko136t/%D0%BE%D1%82%D0%B2%D0%B5%D1%87%D0%B0%D0%B5%D0%BC-%D0%BD%D0%B0-%D0%B2%D0%B0%D1%88%D0%B8-%D0%B2%D0%BE%D0%BF%D1%80%D0%BE%D1%81%D1%8B-4a7f75123486'}
+        $.sendMessage('https://medium.com/@savenko136t/%D0%BE%D1%82%D0%B2%D0%B5%D1%87%D0%B0%D0%B5%D0%BC-%D0%BD%D0%B0-%D0%B2%D0%B0%D1%88%D0%B8-%D0%B2%D0%BE%D0%BF%D1%80%D0%BE%D1%81%D1%8B-4a7f75123486')
     }
     get routes() { return { 'company': 'qCompany' } }
 }

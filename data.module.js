1cf6379fcdb10352990cf419b0a25d0e115e7d93
JCompile <Savenko126t@yandex.ru>
2018-10-23 21:47:12 +0300

    dw

--- data.module.js
+++ data.module.js
@@ -92,13 +92,13 @@ module.exports.post_free_admin = {
 module.exports.company = 'Наша компания занимается тонированием и бронирование машин'
 module.exports.copmOpt= {
   reply_markup: JSON.stringify({
-    "inline_keyboard": [[{ "text": "Узнать подробнее","url":'https://medium.com/@savenko136t/%D0%B4%D0%BB%D1%8F-%D1%86%D0%B5%D0%BD%D0%B8%D1%82%D0%B5%D0%BB%D0%B5%D0%B9-%D0%BE%D1%80%D0%B8%D0%B3%D0%B8%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8-24dd3eab2b65' }]],
+    "inline_keyboard": [[{ "text": "Узнать подробнее","url":"https://medium.com/@savenko136t/%D0%B4%D0%BB%D1%8F-%D1%86%D0%B5%D0%BD%D0%B8%D1%82%D0%B5%D0%BB%D0%B5%D0%B9-%D0%BE%D1%80%D0%B8%D0%B3%D0%B8%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8-24dd3eab2b65" }]],
  })
 };
 
 module.exports.qwest= 'Отвечаем на вопросы'
 module.exports.qwestOpt= {
   reply_markup: JSON.stringify({
-    "inline_keyboard": [[{ "text": "Читать ответы","url":' https://medium.com/@savenko136t/%D0%BE%D1%82%D0%B2%D0%B5%D1%87%D0%B0%D0%B5%D0%BC-%D0%BD%D0%B0-%D0%B2%D0%B0%D1%88%D0%B8-%D0%B2%D0%BE%D0%BF%D1%80%D0%BE%D1%81%D1%8B-4a7f75123486' }]],
+    "inline_keyboard": [[{ "text": "Читать ответы","url":"https://medium.com/@savenko136t/%D0%BE%D1%82%D0%B2%D0%B5%D1%87%D0%B0%D0%B5%D0%BC-%D0%BD%D0%B0-%D0%B2%D0%B0%D1%88%D0%B8-%D0%B2%D0%BE%D0%BF%D1%80%D0%BE%D1%81%D1%8B-4a7f75123486"}]],
  })
 };

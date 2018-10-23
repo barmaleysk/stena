c26c11cd3024909fe459b7635db8e769096c7117
JCompile <Savenko126t@yandex.ru>
2018-10-23 06:51:23 +0300

    фысфы

--- v2.js
+++ v2.js
@@ -120,7 +120,7 @@ class SaleController extends TelegramBaseController {
 class Qwest extends TelegramBaseController {
     qQwest($) { 
         //analytic.add('О компании', $); 
-        $.sendMessage('https://medium.com/@savenko136t/%D0%BE%D1%82%D0%B2%D0%B5%D1%87%D0%B0%D0%B5%D0%BC-%D0%BD%D0%B0-%D0%B2%D0%B0%D1%88%D0%B8-%D0%B2%D0%BE%D0%BF%D1%80%D0%BE%D1%81%D1%8B-4a7f75123486')
+        $.sendMessage('Чем мы занимаемся https://medium.com/@savenko136t/%D0%BE%D1%82%D0%B2%D0%B5%D1%87%D0%B0%D0%B5%D0%BC-%D0%BD%D0%B0-%D0%B2%D0%B0%D1%88%D0%B8-%D0%B2%D0%BE%D0%BF%D1%80%D0%BE%D1%81%D1%8B-4a7f75123486')
     }
     get routes() { return { 'qwest': 'qQwest' } }
 }
